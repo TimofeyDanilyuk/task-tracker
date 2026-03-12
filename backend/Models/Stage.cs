3b0802c1016ace4aee6014bd293be9232e0c37a8
Тимофей Данилюк <timofei.danilyuk@gmail.com>
2026-03-12 09:07:52 +0700

    fix UserId type int

--- backend/Models/Stage.cs
+++ backend/Models/Stage.cs
@@ -5,6 +5,6 @@ public class Stage
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Color { get; set; } = "#3498db"; // HEX цвет
-    public string? UserId { get; set; }
+    public int? UserId { get; set; }
     public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
 }
